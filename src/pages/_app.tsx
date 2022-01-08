08d3f597b6f7c71926891e0294f93f70cc4ee7dc
JeongHoon Park <kmclo@naver.com>
2022-01-08 13:00:45 +0900

    :lipstick: Global style 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css';
+import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import { GlobalStyles } from '../styles/globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={GlobalStyles} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
