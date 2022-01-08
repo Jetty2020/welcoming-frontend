1df2855d731de00641b6f49e2ffe4eefaf1cae96
JeongHoon Park <kmclo@naver.com>
2022-01-08 12:59:56 +0900

    :bento: Noto sans KR 폰트 추가

---
+++ src/_document.tsx
@@ -0,0 +1,40 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            property="og:url"
+            content="https://futurama-fan-site.vercel.app/"
+          />
+          <meta property="og:title" content="Welcoming site" />
+          <meta property="og:description" content="Enjoy home decorating" />
+          <meta property="og:image" content="/vercel.svg" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
