538257b4a2e70bde8da728df3c99dbbf97c045de
JeongHoon Park <kmclo@naver.com>
2022-01-15 02:31:02 +0900

    :fire: 필요없는 THEME, MEDIA_QUERY_END_POINT 상수 삭제

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,14 +1,1 @@
 export const SERVER_URL = 'http://localhost:5050/graphql';
-
-export const THEME = {
-  accent: '#34C5EF',
-  while: '#fff',
-  black: '#000',
-};
-
-export const MEDIA_QUERY_END_POINT = {
-  MOBILE: '375px',
-  TABLET: '768px',
-  LAPTOP: '1366px',
-  DESKTOP: '1600px',
-};
