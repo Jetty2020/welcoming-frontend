a1516d564109a76e3df82cac2b72950c4e7f50be
JeongHoon Park <kmclo@naver.com>
2022-01-15 22:23:41 +0900

    :sparkles: 다크모드 토글 버튼 구현

--- src/styles/CustomThemeProvider.tsx
+++ src/styles/CustomThemeProvider.tsx
@@ -26,9 +26,9 @@ export const CustomThemeProvider: React.FC = ({ children }) => {
   const body = (
     <ThemeProvider theme={theme}>
       {children}
-      <button type="button" onClick={toggleTheme}>
-        다크모드
-      </button>
+      <DarkModeBtn type="button" onClick={toggleTheme}>
+        {dark ? '다크 모드' : '라이트 모드'}
+      </DarkModeBtn>
     </ThemeProvider>
   );
 
@@ -37,3 +37,15 @@ export const CustomThemeProvider: React.FC = ({ children }) => {
   }
   return body;
 };
+
+const DarkModeBtn = styled.button`
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  width: 75px;
+  height: 75px;
+  border-radius: 30%;
+  background: ${({ theme }) => theme.bg.darkBtn};
+  color: ${({ theme }) => theme.text.darkBtn};
+  font-weight: 600;
+`;
