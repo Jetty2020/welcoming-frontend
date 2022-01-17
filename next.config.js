645e1096052dfe7dc6ff0a59ccc8c3458f460951
JeongHoon Park <kmclo@naver.com>
2022-01-17 18:49:00 +0900

    :wrench: next config 파일 images domain 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} **/
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['welcoming-2022.s3.amazonaws.com'],
+  },
+};
