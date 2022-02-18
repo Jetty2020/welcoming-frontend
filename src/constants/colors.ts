4282ac8f4908137d0166f7df5de45a7a96639209
Bori-github <qhflrnfl4324@gmail.com>
2022-02-19 00:31:57 +0900

    :lipstick: color palette 추가

---
+++ src/constants/colors.ts
@@ -0,0 +1,22 @@
+export const PRIMARY_900 = '#FF9100';
+export const PRIMARY_800 = '#FF9C1B';
+export const PRIMARY_700 = '#FFA734';
+export const PRIMARY_600 = '#FFB24D';
+export const PRIMARY_500 = '#FFBD66';
+export const PRIMARY_400 = '#FFC880';
+export const PRIMARY_300 = '#FFD399';
+export const PRIMARY_200 = '#FFDEB2';
+export const PRIMARY_100 = '#FFE9CB';
+export const PRIMARY_50 = '#FFFAF2';
+
+export const GRAY_900 = '#212121';
+export const GRAY_800 = '#424242';
+export const GRAY_700 = '#616161';
+export const GRAY_600 = '#757575';
+export const GRAY_500 = '#9E9E9E';
+export const GRAY_400 = '#BDBDBD';
+export const GRAY_300 = '#E0E0E0';
+export const GRAY_200 = '#EEEEEE';
+export const GRAY_100 = '#F5F5F5';
+export const GRAY_50 = '#FAFAFA';
+export const WHITE = '#FFFFFF';
