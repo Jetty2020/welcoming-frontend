e2d32aa3451fb6431359329e51ec983da2bc9531
JeongHoon Park <kmclo@naver.com>
2022-02-18 12:10:29 +0900

    :truck: utils 폴더 pages 폴더 밖으로 이동

--- src/utils/pxToRem.tsx
+++ src/utils/pxToRem.tsx
@@ -1,4 +1,4 @@
-import { ROOT_FONT_SIZE } from '../../constants';
+import { ROOT_FONT_SIZE } from '../constants';
 
 export const pxToRem = (num: number) => {
   return `${(num / ROOT_FONT_SIZE).toFixed(3)}rem`;
