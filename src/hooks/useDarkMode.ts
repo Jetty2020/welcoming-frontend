a9b872967403a75f6603cab5f653cd0f3705bb69
JeongHoon Park <kmclo@naver.com>
2022-02-20 21:29:44 +0900

    :recycle: useDarkMode hook 추상화

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useState } from 'react';
+import { mode } from '@styles/theme';
+
+export const useDarkMode = () => {
+  const [mounted, setMounted] = useState(false);
+  const [theme, setTheme] = useState(mode.light);
+  const [dark, setDark] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+
+    if (
+      window.localStorage.getItem('welcoming-theme') === 'dark' ||
+      (window.matchMedia('(prefers-color-scheme: dark)').matches &&
+        !window.localStorage.getItem('welcoming-theme'))
+    ) {
+      setDark(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem(
+      'welcoming-theme',
+      `${dark ? 'dark' : 'light'}`,
+    );
+    if (window.localStorage.getItem('welcoming-theme') === 'dark') {
+      setTheme(mode.dark);
+    } else if (window.localStorage.getItem('welcoming-theme') === 'light') {
+      setTheme(mode.light);
+    }
+  }, [dark]);
+
+  const toggleTheme = useCallback(() => {
+    setDark((curr) => !curr);
+  }, [dark]);
+
+  return { dark, theme, mounted, toggleTheme };
+};
