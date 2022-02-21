564a20f695473d7c1801a128c2c6e777afad0a49
JeongHoon Park <kmclo@naver.com>
2022-02-22 01:26:19 +0900

    :fire: next config에서 svg 파일 설정 코드 삭제

--- next.config.js
+++ next.config.js
@@ -4,11 +4,4 @@ module.exports = {
   images: {
     domains: ['welcoming-2022.s3.amazonaws.com'],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
-  },
 };
