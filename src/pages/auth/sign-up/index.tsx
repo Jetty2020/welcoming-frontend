da4cc92e8e775f1683b11c8786c77dfabe59aa4f
JeongHoon Park <kmclo@naver.com>
2022-03-01 00:20:57 +0900

    :construction: sign-up page UI 구현

--- src/pages/auth/sign-up/index.tsx
+++ src/pages/auth/sign-up/index.tsx
@@ -11,6 +11,7 @@ import {
 } from '@generated/createAccountMutation';
 import { pxToRem } from '@utils/pxToRem';
 import { Close } from 'public/icons';
+import { GRAY_500, PRIMARY_900, WHITE } from '@constants/colors';
 
 const CREATE_ACCOUNT_MUTATION = gql`
   mutation createAccountMutation($createAccountInput: CreateAccountInput!) {
@@ -33,7 +34,7 @@ const SignUp: NextPage = () => {
     }
   };
 
-  const [loginMutation, { loading }] = useMutation<
+  const [createAccountMutation, { loading }] = useMutation<
     createAccountMutation,
     createAccountMutationVariables
   >(CREATE_ACCOUNT_MUTATION, {
@@ -55,69 +56,72 @@ const SignUp: NextPage = () => {
         <Contlogo>
           <img src="/logo/logo.png" alt="어서와 우리집 로고" />
         </Contlogo>
-        <form>
-          <label htmlFor="emailDomain">
+        <Form>
+          <Label htmlFor="emailDomain">
             이메일
-            <input type="text" placeholder="이메일" />
-          </label>
-          <span>필수 입력 항목입니다.</span>
-          <span>이메일 형식이 올바르지 않습니다.</span>
-          <span>이미 가입한 이메일입니다.</span>
-          <label htmlFor="password">
+            <Input type="text" placeholder="이메일" />
+          </Label>
+          <Error>필수 입력 항목입니다.</Error>
+          <Error>이메일 형식이 올바르지 않습니다.</Error>
+          <Error>이미 가입한 이메일입니다.</Error>
+          <Label htmlFor="password">
             비밀번호
-            <span>
-              비밀번호는 영문(대/소문자 구분), 숫자 조합하여 6~12자리로
-              입력해주세요.
-            </span>
-            <input type="password" placeholder="비밀번호" />
-          </label>
-          <span>
+            <Desc>영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.</Desc>
+            <Input type="password" placeholder="비밀번호" />
+          </Label>
+          <Error>
             비밀번호는 영문(대/소문자 구분), 숫자 조합하여 6~12자리이어야
             합니다.
-          </span>
-          <label htmlFor="passwordCheck">
+          </Error>
+          <Label htmlFor="passwordCheck">
             비밀번호 확인
-            <input type="password" placeholder="비밀번호 확인" />
-          </label>
-          <span>비밀번호가 일치하지 않습니다.</span>
-          <label htmlFor="nickName">
+            <Input type="password" placeholder="비밀번호 확인" />
+          </Label>
+          <Error>비밀번호가 일치하지 않습니다.</Error>
+          <Label htmlFor="nickName">
             별명
-            <span>다른 유저와 겹치지 않는 닉네임을 입력해주세요.(2~15자)</span>
-            <input type="text" placeholder="닉네임 (2~15자)" />
-          </label>
-          <span>사용 중인 별명입니다.</span>
-          <span>별명은 2~15자리이어야 합니다.</span>
-          <div>
+            <Desc>다른 유저와 겹치지 않는 별명을 입력해주세요. (2~15자)</Desc>
+            <Input type="text" placeholder="닉네임 (2~15자)" />
+          </Label>
+          <Error>사용 중인 별명입니다.</Error>
+          <Error>별명은 2~15자리이어야 합니다.</Error>
+          <ContainerAgreement>
             약관동의
-            <div>
-              <label htmlFor="agreeAll">
-                <input type="checkbox" id="agreeAll" />
+            <InnerContainerAgreement>
+              <LabelAgreement htmlFor="agreeAll">
+                <CheckBox type="checkbox" id="agreeAll" />
                 전체동의
-              </label>
-              <label htmlFor="agreeAll">
-                <input type="checkbox" id="agreeAll" />만 14세 이상이니다.
-                <span>&#40;필수&#41;</span>
-              </label>
-              <label htmlFor="agreeAll">
-                <input type="checkbox" id="agreeAll" />
-                이용약관
-                <span>&#40;필수&#41;</span>
-              </label>
-              <label htmlFor="agreeAll">
-                <input type="checkbox" id="agreeAll" />
-                개인정보수집 및 이용동의
-                <span>&#40;필수&#41;</span>
-              </label>
-            </div>
-          </div>
-          <button type="submit">회원가입하기</button>
-        </form>
-        <p>
+              </LabelAgreement>
+              <LabelAgreement htmlFor="agreeAge">
+                <CheckBox type="checkbox" id="agreeAge" />만 14세 이상입니다.
+                <SpanRequire>&#40;필수&#41;</SpanRequire>
+              </LabelAgreement>
+              <LabelAgreement htmlFor="agreeTerm">
+                <CheckBox type="checkbox" id="agreeTerm" />
+                <Link href="/" passHref>
+                  <LinkAgreeMent target="_blank">이용약관</LinkAgreeMent>
+                </Link>
+                <SpanRequire>&#40;필수&#41;</SpanRequire>
+              </LabelAgreement>
+              <LabelAgreement htmlFor="agreePrivacy">
+                <CheckBox type="checkbox" id="agreePrivacy" />
+                <Link href="/" passHref>
+                  <LinkAgreeMent target="_blank">
+                    개인정보수집 및 이용동의
+                  </LinkAgreeMent>
+                </Link>
+                <SpanRequire>&#40;필수&#41;</SpanRequire>
+              </LabelAgreement>
+            </InnerContainerAgreement>
+          </ContainerAgreement>
+          <Button type="submit">회원가입하기</Button>
+        </Form>
+        <TextAskID>
           이미 아이디가 있으신가요?
-          <Link href="login">
-            <a>로그인</a>
+          <Link href="/auth/sign-in" passHref>
+            <AskIDLink>로그인</AskIDLink>
           </Link>
-        </p>
+        </TextAskID>
       </SectionSignUp>
       <AuthFooter />
     </MainSignUp>
@@ -164,8 +168,9 @@ const SectionSignUp = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  box-sizing: border-box;
   width: 100%;
-  max-width: ${pxToRem(300)};
+  max-width: ${pxToRem(450)};
   padding: ${pxToRem(30)};
   margin: ${pxToRem(50)} 0;
   flex-grow: 1;
@@ -176,11 +181,34 @@ const Contlogo = styled.div`
   margin: 0 auto ${pxToRem(50)};
 `;
 
-const InputSyle = styled.input`
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const Desc = styled.span`
+  margin-top: 12px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.4;
+  color: ${GRAY_500};
+`;
+
+const Input = styled.input`
   width: 100%;
+  margin-top: 12px;
   padding: ${pxToRem(15)};
   box-sizing: border-box;
   border: ${({ theme }) => theme.input.border};
+  border-radius: ${pxToRem(5)};
   font-size: ${pxToRem(16)};
 
   &::placeholder {
@@ -191,6 +219,108 @@ const InputSyle = styled.input`
   }
 `;
 
-const InputEmail = styled(InputSyle)`
-  border-radius: ${pxToRem(5)} ${pxToRem(5)} 0 0;
+const Error = styled.span`
+  /* display: none; */
+  margin-top: ${pxToRem(10)};
+  color: ${({ theme }) => theme.input.error};
+  font-size: ${pxToRem(14)};
+  line-height: 1.4;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    min-width: ${pxToRem(14)};
+    height: ${pxToRem(14)};
+    margin: 0 ${pxToRem(5)} ${pxToRem(-2)} 0;
+    background: url('/icons/emoji-frown.svg') no-repeat;
+    background-size: 100%;
+  }
+`;
+
+const ContainerAgreement = styled.div`
+  font-size: 15px;
+  font-weight: 700;
+`;
+
+const InnerContainerAgreement = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px 0;
+  padding: 20px 10px 10px;
+  border: ${({ theme }) => theme.input.border};
+  background-color: ${WHITE};
+`;
+
+const LabelAgreement = styled.label`
+  display: flex;
+  align-items: center;
+  margin: 6px 0;
+  font-weight: 500;
+
+  &[for='agreeAll'] {
+    margin-top: 0;
+    padding-bottom: 20px;
+    border-bottom: ${({ theme }) => theme.input.border};
+    font-weight: 700;
+  }
+`;
+
+const LinkAgreeMent = styled.a`
+  text-decoration: underline;
+`;
+
+const SpanRequire = styled.span`
+  margin-left: 5px;
+  color: ${PRIMARY_900};
+`;
+
+const CheckBox = styled.input`
+  display: grid;
+  place-content: center;
+  appearance: none;
+  background-color: #fff;
+  width: 18px;
+  height: 18px;
+  margin-right: 10px;
+  border: 1px solid ${GRAY_500};
+  border-radius: 2px;
+
+  &:checked {
+    border: none;
+    background-color: ${({ theme }) => theme.background.primary};
+  }
+  &:checked::before {
+    content: '';
+    width: 14px;
+    height: 14px;
+    border-radius: 1px;
+    box-shadow: inset 14px 14px ${WHITE};
+    clip-path: polygon(15% 41%, 44% 65%, 83% 10%, 95% 20%, 47% 88%, 5% 54%);
+  }
+`;
+
+const Button = styled.button`
+  width: 100%;
+  margin: ${pxToRem(35)} 0 ${pxToRem(30)};
+  padding: ${pxToRem(15)};
+  border-radius: ${pxToRem(5)};
+  background-color: ${({ theme }) => theme.button.background};
+  font-size: ${({ theme }) => pxToRem(theme.button.fontSize)};
+  font-weight: ${({ theme }) => theme.button.fontWeight};
+  letter-spacing: ${({ theme }) => theme.button.letterSpacing};
+  color: ${({ theme }) => theme.button.text};
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.button.disabled};
+  }
+`;
+
+const TextAskID = styled.p`
+  text-align: center;
+`;
+
+const AskIDLink = styled.a`
+  margin-left: 5px;
+  font-weight: 600;
+  text-decoration: underline;
 `;
