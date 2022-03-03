9c25db222abb21d44395d37eecdcebe49331c35c
JeongHoon Park <kmclo@naver.com>
2022-03-03 22:53:37 +0900

    :lipstick: progress bar UI 구현

--- src/components/landing/WelcomeDeal.tsx
+++ src/components/landing/WelcomeDeal.tsx
@@ -1,3 +1,4 @@
+import { GRAY_500, PRIMARY_700 } from '@constants/colors';
 import styled from '@emotion/styled';
 import { pxToRem } from '@utils/pxToRem';
 import Link from 'next/link';
@@ -18,10 +19,11 @@ export const WelcomeDeal = () => {
         </Link>
       </ContainerTitle>
       <ContainerCountDay>
-        <Label htmlFor="countDay">
-          <Progress id="countDay" max="100" value="70" />
-          D-Day
-        </Label>
+        <ProgressBar>
+          <ProgressValue progress-value={10} />
+        </ProgressBar>
+        <TextCountDay>D-Day</TextCountDay>
+        <span>2022.02.21 ~ 03.12</span>
       </ContainerCountDay>
     </Section>
   );
@@ -30,7 +32,7 @@ export const WelcomeDeal = () => {
 const Section = styled.section`
   margin: ${pxToRem(40)} auto 0;
   max-width: ${pxToRem(1200)};
-  padding: 0 15px;
+  padding: 0 ${pxToRem(15)};
 `;
 
 const ContainerTitle = styled.div`
@@ -40,42 +42,48 @@ const ContainerTitle = styled.div`
 `;
 
 const Title = styled.h2`
-  margin-bottom: 5px;
+  margin-bottom: ${pxToRem(5)};
   font-size: ${({ theme }) => pxToRem(theme.title.fontSize)};
   font-weight: ${({ theme }) => theme.title.fontWeight};
 `;
 
 const SubTitle = styled.p`
   color: ${({ theme }) => theme.text.lighter};
-  font-size: 14px;
+  font-size: ${pxToRem(14)};
 `;
 
 const AnchorMore = styled.a`
   display: flex;
   align-items: center;
-  font-size: 14px;
+  font-size: ${pxToRem(14)};
   font-weight: 600;
 
   & svg {
-    width: 14px;
-    margin-left: 5px;
+    width: ${pxToRem(14)};
+    margin-left: ${pxToRem(5)};
     vertical-align: baseline;
   }
 `;
 
 const ContainerCountDay = styled.div`
-  margin: 10px 0;
+  margin: ${pxToRem(10)} 0;
+  font-weight: 700;
 `;
 
-const Label = styled.label`
-  display: grid;
-  grid-template-columns: 85% 15%;
+const ProgressBar = styled.div`
   width: 100%;
+  height: 2px;
+  margin-bottom: ${pxToRem(10)};
+  background-color: ${GRAY_500};
+`;
+
+const TextCountDay = styled.span`
+  margin-right: ${pxToRem(10)};
   color: ${({ theme }) => theme.text.primary};
-  font-size: 14px;
-  text-align: right;
 `;
 
-const Progress = styled.progress`
-  width: 100%;
+const ProgressValue = styled.div<{ 'progress-value': number }>`
+  width: ${(props) => props['progress-value']}%;
+  height: 100%;
+  background-color: ${PRIMARY_700};
 `;
