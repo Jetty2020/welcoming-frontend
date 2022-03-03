31b7de6327bf0409e92cea9e174fc9660f071fe0
Bori-github <qhflrnfl4324@gmail.com>
2022-03-03 23:57:55 +0900

    :lipstick: 웰컴딜 판매 상품 리스트 UI 구현

--- src/components/landing/WelcomeDeal.tsx
+++ src/components/landing/WelcomeDeal.tsx
@@ -1,8 +1,18 @@
-import { GRAY_500, PRIMARY_700 } from '@constants/colors';
+import {
+  ERROR,
+  GRAY_300,
+  GRAY_500,
+  PRIMARY_700,
+  PRIMARY_800,
+} from '@constants/colors';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { pxToRem } from '@utils/pxToRem';
 import Link from 'next/link';
+import { v4 as uuidv4 } from 'uuid';
+import { pxToRem } from '@utils/pxToRem';
 import { ChevronRight } from 'public/icons';
+
+const data = [0, 1, 2, 3, 4, 5, 6];
 
 export const WelcomeDeal = () => {
   return (
@@ -25,6 +35,35 @@ export const WelcomeDeal = () => {
         <TextCountDay>D-Day</TextCountDay>
         <span>2022.02.21 ~ 03.12</span>
       </ContainerCountDay>
+      <List>
+        {data.map((item) => {
+          return (
+            <Item key={`product-list-${uuidv4()}`}>
+              <Link href="/" passHref>
+                <Anchor>
+                  <Img>이미지 준비중</Img>
+                  <Info>
+                    <BrandName>브랜드 이름</BrandName>
+                    <ProductName>
+                      [상품명] 낮엔 소파, 밤엔 침대! 접이식 인기 소파베드 모음
+                    </ProductName>
+                    <Discount>50%</Discount>
+                    <Price>157,000원</Price>
+                    <ContainerReview>
+                      <Rate>별점 4.5 </Rate>
+                      <Review>리뷰 14,623</Review>
+                    </ContainerReview>
+                    <div>
+                      <TagFreeShipping>무료배송</TagFreeShipping>
+                      <TagEventPrice>특가</TagEventPrice>
+                    </div>
+                  </Info>
+                </Anchor>
+              </Link>
+            </Item>
+          );
+        })}
+      </List>
     </Section>
   );
 };
@@ -66,7 +105,7 @@ const AnchorMore = styled.a`
 `;
 
 const ContainerCountDay = styled.div`
-  margin: ${pxToRem(10)} 0;
+  margin: ${pxToRem(10)} 0 ${pxToRem(30)};
   font-weight: 700;
 `;
 
@@ -86,4 +125,97 @@ const ProgressValue = styled.div<{ 'progress-value': number }>`
   width: ${(props) => props['progress-value']}%;
   height: 100%;
   background-color: ${PRIMARY_700};
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Item = styled.li`
+  & + & {
+    margin-top: ${pxToRem(20)};
+  }
+
+  & + &::before {
+    content: '';
+    display: block;
+    margin-bottom: ${pxToRem(20)};
+    border-top: 1px solid ${GRAY_300};
+  }
+`;
+
+const Anchor = styled.a`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: ${pxToRem(10)};
+  min-height: calc(50vw - ${pxToRem(20)});
+`;
+
+const Img = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #ddd;
+`;
+
+const Info = styled.div`
+  align-self: center;
+`;
+
+const BrandName = styled.span`
+  display: inline-block;
+  margin-top: ${pxToRem(5)};
+  color: ${({ theme }) => theme.text.lighter};
+  font-size: ${pxToRem(13)};
+  font-weight: 600;
+`;
+
+const ProductName = styled.p`
+  margin: ${pxToRem(10)} 0;
+  line-height: 1.3;
+`;
+
+const Discount = styled.span`
+  padding-right: ${pxToRem(5)};
+  color: ${PRIMARY_800};
+  font-size: ${pxToRem(17)};
+  font-weight: 600;
+`;
+
+const Price = styled.span`
+  font-size: ${pxToRem(17)};
+  font-weight: 600;
+`;
+
+const ContainerReview = styled.div`
+  margin: ${pxToRem(10)} 0;
+  font-size: ${pxToRem(12)};
+`;
+
+const Rate = styled.span`
+  color: ${PRIMARY_800};
+`;
+
+const Review = styled.span`
+  color: ${({ theme }) => theme.text.lighter};
+`;
+
+const StyleTag = () => css`
+  display: inline-block;
+  margin: 0 ${pxToRem(5)} ${pxToRem(5)} 0;
+  padding: ${pxToRem(5)} ${pxToRem(8)};
+  border: 1px solid transparent;
+  border-radius: ${pxToRem(20)};
+  font-size: ${pxToRem(11)};
+`;
+
+const TagFreeShipping = styled.span`
+  ${StyleTag}
+  border-color: ${({ theme }) => theme.text.default};
+`;
+
+const TagEventPrice = styled.span`
+  ${StyleTag}
+  background-color: ${ERROR};
+  color: ${({ theme }) => theme.background.default};
 `;
