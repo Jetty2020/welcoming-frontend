528abac728f8b87d6a343a0ad90e1d3be10de0f8
Bori-github <qhflrnfl4324@gmail.com>
2022-03-04 13:00:12 +0900

    :wrench: svgr webpack 설정

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ module.exports = {
   images: {
     domains: ['welcoming-2022.s3.amazonaws.com'],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
