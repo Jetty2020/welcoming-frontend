6fa8f8c1630a843caf3080e5b6b7e41eeb0a853b
Bori-github <qhflrnfl4324@gmail.com>
2022-03-05 11:44:54 +0900

    :lipstick: 베스트 섹션 모바일 UI 구현

--- src/components/landing/Best.tsx
+++ src/components/landing/Best.tsx
@@ -1,10 +1,15 @@
 import styled from '@emotion/styled';
 import Link from 'next/link';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { css } from '@emotion/react';
 import { pxToRem } from '@utils/pxToRem';
 import { ChevronRight } from 'public/icons';
 import Caret from 'public/icons/caret-down.svg';
 import { Modal } from '@components/common/Modal';
+import { PRIMARY_800, ERROR, PRIMARY_600 } from '@constants/colors';
+
+const fakeData = [0, 1, 2, 3, 4, 5, 6];
 
 export const Best = () => {
   const [isShowModal, setIsShowModal] = useState(false);
@@ -12,7 +17,10 @@ export const Best = () => {
   return (
     <Section>
       <ContainerTitle>
-        <Title>베스트</Title>
+        <div>
+          <Title>베스트</Title>
+          <SubTitle>어서와 우리집 베스트 TOP 100</SubTitle>
+        </div>
         <Link href="/" passHref>
           <AnchorMore>
             더보기 <ChevronRight />
@@ -45,6 +53,35 @@ export const Best = () => {
           </ul>
         </>
       </Modal>
+      <List>
+        {fakeData.map((item) => {
+          return (
+            <li key={`best-list-${uuidv4()}`}>
+              <Link href="/" passHref>
+                <a>
+                  <Img>이미지 준비중</Img>
+                  <Info>
+                    <BrandName>브랜드 이름</BrandName>
+                    <ProductName>
+                      [상품명] 낮엔 소파, 밤엔 침대! 접이식 인기 소파베드 모음
+                    </ProductName>
+                    <Discount>50%</Discount>
+                    <Price>157,000원</Price>
+                    <ContainerReview>
+                      <Rate>별점 4.5 </Rate>
+                      <Review>리뷰 14,623</Review>
+                    </ContainerReview>
+                    <div>
+                      <TagFreeShipping>무료배송</TagFreeShipping>
+                      <TagEventPrice>특가</TagEventPrice>
+                    </div>
+                  </Info>
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </List>
     </Section>
   );
 };
@@ -58,12 +95,28 @@ const ContainerTitle = styled.div`
   display: flex;
   align-items: flex-start;
   justify-content: space-between;
+  position: relative;
+
+  &::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: ${pxToRem(2)};
+    position: absolute;
+    bottom: ${pxToRem(-12)};
+    background-color: ${PRIMARY_600};
+  }
 `;
 
 const Title = styled.h2`
   margin-bottom: ${pxToRem(5)};
   font-size: ${({ theme }) => pxToRem(theme.title.mobileFontSize)};
   font-weight: ${({ theme }) => theme.title.fontWeight};
+`;
+
+const SubTitle = styled.p`
+  color: ${({ theme }) => theme.text.lighter};
+  font-size: ${pxToRem(14)};
 `;
 
 const AnchorMore = styled.a`
@@ -80,9 +133,9 @@ const AnchorMore = styled.a`
 `;
 
 const BtnOpenFilter = styled.button`
-  margin-top: ${pxToRem(10)};
+  margin-top: ${pxToRem(22)};
   padding: ${pxToRem(6)} ${pxToRem(6)} ${pxToRem(5)} ${pxToRem(10)};
-  border-radius: ${pxToRem(20)};
+  border-radius: ${pxToRem(4)};
   background-color: ${({ theme }) => theme.text.default};
   font-size: ${pxToRem(14)};
   color: ${({ theme }) => theme.background.default};
@@ -105,4 +158,79 @@ const ItemFilter = styled.li`
 
 const BtnFilter = styled.button`
   font-size: ${pxToRem(16)};
+`;
+
+const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: ${pxToRem(20)} ${pxToRem(10)};
+  margin-top: ${pxToRem(10)};
+`;
+
+const Img = styled.div`
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  background-color: #ddd;
+`;
+
+const Info = styled.div`
+  padding: ${pxToRem(10)} ${pxToRem(5)};
+`;
+
+const BrandName = styled.span`
+  display: inline-block;
+  margin-top: ${pxToRem(5)};
+  color: ${({ theme }) => theme.text.lighter};
+  font-size: ${pxToRem(13)};
+  font-weight: 600;
+`;
+
+const ProductName = styled.p`
+  margin: ${pxToRem(10)} 0;
+  line-height: 1.3;
+`;
+
+const Discount = styled.span`
+  padding-right: ${pxToRem(5)};
+  color: ${PRIMARY_800};
+  font-size: ${pxToRem(17)};
+  font-weight: 600;
+`;
+
+const Price = styled.span`
+  font-size: ${pxToRem(17)};
+  font-weight: 600;
+`;
+
+const ContainerReview = styled.div`
+  margin: ${pxToRem(10)} 0;
+  font-size: ${pxToRem(12)};
+`;
+
+const Rate = styled.span`
+  color: ${PRIMARY_800};
+`;
+
+const Review = styled.span`
+  color: ${({ theme }) => theme.text.lighter};
+`;
+
+const StyleTag = () => css`
+  display: inline-block;
+  margin: 0 ${pxToRem(5)} ${pxToRem(5)} 0;
+  padding: ${pxToRem(5)} ${pxToRem(8)};
+  border: 1px solid transparent;
+  border-radius: ${pxToRem(20)};
+  font-size: ${pxToRem(11)};
+`;
+
+const TagFreeShipping = styled.span`
+  ${StyleTag}
+  border-color: ${({ theme }) => theme.text.default};
+`;
+
+const TagEventPrice = styled.span`
+  ${StyleTag}
+  background-color: ${ERROR};
+  color: ${({ theme }) => theme.background.default};
 `;
