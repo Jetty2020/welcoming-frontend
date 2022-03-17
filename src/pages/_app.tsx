27d7e70584e9eccf1bd38c2e0552dc721395029e
JeongHoon Park <kmclo@naver.com>
2022-03-17 12:06:52 +0900

    :sparkles: 매 페이지마다 로그인 상태를 트래킹하도록 기능 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import Cookies from 'js-cookie';
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import { CustomThemeProvider } from '@components/common/DarkMode/CustomThemeProvider';
-import client from '../apollo';
+import client, { isLoggedInVar, authTokenVar } from '@apollo';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const token = Cookies.get('token');
+
+  isLoggedInVar(Boolean(token));
+  authTokenVar(token);
+
   return (
     <ApolloProvider client={client}>
       <CustomThemeProvider>
