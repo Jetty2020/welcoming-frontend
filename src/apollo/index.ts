b9fe307b558469d2cfd97df48f4979921d91fb57
JeongHoon Park <kmclo@naver.com>
2022-03-17 23:02:24 +0900

    :adhesive_bandage: Token 값 상수 적용

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -1,14 +1,15 @@
 import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
 import Cookies from 'js-cookie';
-import { SERVER_URL } from '../constants';
+import { SERVER_URL, TOKEN_KEY } from '../constants';
 
-const token = Cookies.get('token');
+const token = Cookies.get(TOKEN_KEY);
+
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 export const isDark = makeVar(false);
 
 export const userLogout = () => {
-  Cookies.remove('token');
+  Cookies.remove(TOKEN_KEY);
   isLoggedInVar(false);
   authTokenVar('');
   client.cache.reset();
