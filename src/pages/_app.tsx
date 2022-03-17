218e535a1b63116083863dd1dd21955dbcfe8c45
Bori-github <qhflrnfl4324@gmail.com>
2022-03-17 22:37:12 +0900

    :fire: 불필요한 코드 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,9 @@
-import Cookies from 'js-cookie';
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import { CustomThemeProvider } from '@components/common/DarkMode/CustomThemeProvider';
-import client, { isLoggedInVar, authTokenVar } from '@apollo';
-import { TOKEN_KEY } from '@constants/index';
+import { client } from '@apollo';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const token = Cookies.get(TOKEN_KEY);
-
-  isLoggedInVar(Boolean(token));
-  authTokenVar(token);
-
   return (
     <ApolloProvider client={client}>
       <CustomThemeProvider>
