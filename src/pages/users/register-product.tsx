5ba51a7277d4d5886b1e4d56efb330f870843d09
JeongHoon Park <kmclo@naver.com>
2022-03-20 17:42:44 +0900

    :lipstick: 상품등록 페이지 UI 구현

--- src/pages/users/register-product.tsx
+++ src/pages/users/register-product.tsx
@@ -1,7 +1,11 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { NextPage } from 'next';
 import PageTitle from '@components/common/PageTitle';
 import { Layout } from '@components/layouts/Layout';
+import { pxToRem } from '@utils/pxToRem';
+import QuestionIcon from 'public/icons/question-circle.svg';
+import { GRAY_200, WHITE } from '@constants/colors';
 
 const RegisterProduct: NextPage = () => {
   return (
@@ -10,126 +14,149 @@ const RegisterProduct: NextPage = () => {
       <Layout>
         <Main>
           <h2 className="sr-only">상품 등록 페이지</h2>
-          <form>
-            <div>
-              <em>브랜드명</em>
-              <select>
-                <option disabled selected>
-                  브랜드 선택
-                </option>
-                <option value="brand1">brand1</option>
-                <option value="brand2">brand2</option>
-                <option value="brand3">brand3</option>
-              </select>
-            </div>
-            <div>
-              <em>상품명</em>
-              <input type="text" />
-            </div>
-            <div>
-              <em>상품 옵션</em>
-              <label htmlFor="use">
-                <input id="use" type="radio" name="option" />
-                <span>사용함</span>
-              </label>
-              <label htmlFor="none">
-                <input id="none" type="radio" name="option" />
-                <span>사용 안함</span>
-              </label>
-            </div>
-            <div>
-              <em>상품 가격</em>
-              <label htmlFor="normalPrice">
-                <span>판매가(원)</span>
-                <input id="normalPrice" type="text" />
-              </label>
-              <label htmlFor="specialPrice">
-                <span>할인 이전 가격(원)</span>
-                <input id="specialPrice" type="text" />
-              </label>
-              <label htmlFor="event">
-                <span>기획전에 추가하기</span>
-                <select id="event">
+          <Form>
+            <TitleForm>
+              브랜드명
+              <IconQuestion />
+            </TitleForm>
+            <Select>
+              <option disabled selected>
+                브랜드 선택
+              </option>
+              <option value="brand1">brand1</option>
+              <option value="brand2">brand2</option>
+              <option value="brand3">brand3</option>
+            </Select>
+            <TitleForm>
+              상품명
+              <IconQuestion />
+            </TitleForm>
+            <Input type="text" />
+            <TitleForm>
+              상품 옵션
+              <IconQuestion />
+            </TitleForm>
+            <ContainerInput>
+              <LabelRadio htmlFor="use">
+                <InputRadio id="use" type="radio" name="option" />
+                사용함
+              </LabelRadio>
+              <LabelRadio htmlFor="none">
+                <InputRadio id="none" type="radio" name="option" />
+                사용 안함
+              </LabelRadio>
+            </ContainerInput>
+            <TitleForm>
+              상품 가격
+              <IconQuestion />
+            </TitleForm>
+            <ContainerInput>
+              <LabelPrice htmlFor="normalPrice">
+                판매가(원)
+                <Input id="normalPrice" type="text" />
+              </LabelPrice>
+              <LabelPrice htmlFor="specialPrice">
+                할인 이전 가격(원)
+                <Input id="specialPrice" type="text" />
+              </LabelPrice>
+              <LabelPrice htmlFor="event">
+                기획전에 추가하기
+                <Select id="event">
                   <option disabled selected>
                     기획전
                   </option>
                   <option value="event1">event1</option>
                   <option value="event2">event2</option>
                   <option value="event3">event3</option>
-                </select>
-              </label>
-            </div>
-            <div>
-              <em>카테고리</em>
-              <select>
+                </Select>
+              </LabelPrice>
+            </ContainerInput>
+            <TitleForm>
+              카테고리
+              <IconQuestion />
+            </TitleForm>
+            <ContainerInput>
+              <Select>
                 <option disabled selected>
                   1차 분류
                 </option>
                 <option value="category1">category1</option>
                 <option value="category2">category2</option>
                 <option value="category3">category3</option>
-              </select>
-              <select>
+              </Select>
+              <Select>
                 <option disabled selected>
                   2차 분류
                 </option>
                 <option value="category1">category1</option>
                 <option value="category2">category2</option>
                 <option value="category3">category3</option>
-              </select>
-            </div>
+              </Select>
+            </ContainerInput>
+            <TitleForm>
+              상품 이미지
+              <IconQuestion />
+            </TitleForm>
             <div>
-              <em>상품 이미지</em>
-              <label htmlFor="imageInput">
-                <span>상품 이미지 추가하기</span>
-                <input id="imageInput" type="file" accept="image/*" multiple />
-              </label>
-              <span>
-                상품 이미지는 최대 5개까지 등록할 수 있습니다. 상품 이미지의 첫
-                번째 이미지는 대표 이미지로 지정됩니다.
-              </span>
-              <ul>
-                <li>
-                  <img src="/logo" alt="상품 이미지 미리보기" />
-                </li>
-              </ul>
+              <LabelImgInput htmlFor="imageInput">
+                상품 이미지 추가하기
+                <input
+                  className="sr-only"
+                  id="imageInput"
+                  type="file"
+                  accept="image/*"
+                  multiple
+                />
+              </LabelImgInput>
+              <ListImg>
+                <ItemImg>
+                  <ImgPreview src="/logo/logo.png" alt="상품 이미지 미리보기" />
+                </ItemImg>
+                <ItemImg>
+                  <ImgPreview src="/logo/logo.png" alt="상품 이미지 미리보기" />
+                </ItemImg>
+                <ItemImg>
+                  <ImgPreview src="/logo/logo.png" alt="상품 이미지 미리보기" />
+                </ItemImg>
+              </ListImg>
             </div>
+            <TitleForm>
+              상품 상세 설명
+              <IconQuestion />
+            </TitleForm>
+            <textarea style={{ height: '300px', resize: 'none' }} />
+            <TitleForm>
+              상품 배송
+              <IconQuestion />
+            </TitleForm>
+            <ContainerInput>
+              <LabelRadio htmlFor="free">
+                <InputRadio id="free" type="radio" name="shippingPrice" />
+                무료배송
+              </LabelRadio>
+              <LabelRadio htmlFor="standard">
+                <InputRadio id="standard" type="radio" name="shippingPrice" />
+                기본 배송비 적용
+              </LabelRadio>
+            </ContainerInput>
+            <TitleForm>
+              검색 엔진 최적화
+              <IconQuestion />
+              <br />
+              (SEO)
+            </TitleForm>
             <div>
-              <em>상품 상세 설명</em>
-              <textarea />
+              <LabelSEO htmlFor="seoTitle">
+                제목
+                <Input id="seoTitle" type="text" />
+              </LabelSEO>
+              <LabelSEO htmlFor="seoDesc">
+                메타 설명
+                <Textarea id="seoDesc" />
+              </LabelSEO>
             </div>
-            <div>
-              <span>상품 배송</span>
-              <label htmlFor="free">
-                <input id="free" type="radio" name="shippingPrice" />
-                <span>무료배송</span>
-              </label>
-              <label htmlFor="standard">
-                <input id="standard" type="radio" name="shippingPrice" />
-                <span>기본 배송비 적용</span>
-              </label>
-              <p>
-                * 무료배송을 선택하면 해당 상품에 ‘무료배송’ 배지가 자동으로
-                생성됩니다.
-              </p>
-            </div>
-            <div>
-              <em>
-                검색 엔진 최적화
-                <br />
-                (SEO)
-              </em>
-              <label htmlFor="seoTitle">
-                <span>제목</span>
-                <input id="seoTitle" type="text" />
-              </label>
-              <label htmlFor="seoDesc">
-                <span>메타 설명</span>
-                <input id="seoDesc" type="text" />
-              </label>
-            </div>
-            <button type="submit">상품 등록하기</button>
-          </form>
+            <BtnSubmit type="submit">상품 등록하기</BtnSubmit>
+          </Form>
         </Main>
       </Layout>
     </>
@@ -139,5 +166,142 @@ const RegisterProduct: NextPage = () => {
 export default RegisterProduct;
 
 const Main = styled.main`
+  max-width: ${pxToRem(800)};
   margin: ${({ theme }) => theme.margin.tabletTop} auto 0;
+  padding: 50px 50px;
+`;
+
+const Form = styled.form`
+  display: grid;
+  gap: 20px 0;
+  grid-template-columns: 150px auto;
+`;
+
+const TitleForm = styled.em`
+  display: inline-block;
+  font-weight: 600;
+`;
+
+const IconQuestion = styled(QuestionIcon)`
+  width: 10px;
+  height: 10px;
+  margin-left: 5px;
+`;
+
+const ContainerInput = styled.div`
+  display: flex;
+`;
+
+const LabelRadio = styled.label`
+  & + & {
+    margin-left: 10px;
+  }
+`;
+
+const InputRadio = styled.input`
+  margin-right: 5px;
+`;
+
+const LableStyle = css`
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+
+  & input,
+  & textarea,
+  & select {
+    margin-top: 10px;
+  }
+`;
+
+const LabelPrice = styled.label`
+  ${LableStyle}
+
+  & + & {
+    margin-left: 10px;
+  }
+`;
+
+const InputStyle = css`
+  box-sizing: border-box;
+  width: 200px;
+  padding: ${pxToRem(5)} ${pxToRem(10)};
+  border-radius: ${pxToRem(5)};
+  font-size: ${pxToRem(16)};
+  // TODO: 컴포넌트화 이후 border 가져오기
+`;
+
+const Input = styled.input`
+  ${InputStyle}
+  border: ${({ theme }) => theme.input.border};
+`;
+
+const Select = styled.select`
+  ${InputStyle}
+  border: ${({ theme }) => theme.input.border};
+
+  & + & {
+    margin-left: 10px;
+  }
+`;
+
+const LabelImgInput = styled.label`
+  display: inline-block;
+  width: 170px;
+  margin-bottom: 20px;
+  border-radius: 15px;
+  background-color: ${GRAY_200};
+  /* color: ${WHITE}; */
+  line-height: 30px;
+  text-align: center;
+  cursor: pointer;
+`;
+
+const ListImg = styled.ul`
+  display: flex;
+`;
+
+const ItemImg = styled.li`
+  & + & {
+    margin-left: 10px;
+  }
+`;
+
+const ImgPreview = styled.img`
+  width: 100px;
+  aspect-ratio: 1/1;
+  object-fit: cover;
+  background-color: ${WHITE};
+`;
+
+const LabelSEO = styled.label`
+  ${LableStyle}
+  & + & {
+    margin-top: 20px;
+  }
+`;
+
+const Textarea = styled.textarea`
+  ${InputStyle}
+  width: 100%;
+  border: ${({ theme }) => theme.input.border};
+  resize: none;
+`;
+
+const BtnSubmit = styled.button`
+  grid-column: 2;
+  grid-row: 10;
+  width: 150px;
+  line-height: 30px;
+  padding: 0 ${pxToRem(15)};
+  border-radius: ${pxToRem(5)};
+  background-color: ${({ theme }) => theme.button.background};
+  font-size: ${({ theme }) => pxToRem(theme.button.fontSize)};
+  font-weight: 500;
+  letter-spacing: ${({ theme }) => theme.button.letterSpacing};
+  color: ${({ theme }) => theme.button.text};
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.button.disabled};
+  }
 `;
