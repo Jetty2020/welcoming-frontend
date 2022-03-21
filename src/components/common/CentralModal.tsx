a0c8cfcacc506d203f08cd115b253c3936fe56d9
JeongHoon Park <kmclo@naver.com>
2022-03-21 22:15:33 +0900

    :sparkles: CentralModal 생성

---
+++ src/components/common/CentralModal.tsx
@@ -0,0 +1,72 @@
+import styled from '@emotion/styled';
+import React, { ReactChild, useEffect } from 'react';
+import { pxToRem } from '@utils/pxToRem';
+
+interface ModalProps {
+  isShowModal: boolean;
+  setIsShowModal: (value: boolean) => void;
+  children: ReactChild;
+  width: string;
+}
+
+interface IsShowModalProps {
+  isShowModal: boolean;
+}
+
+interface ContainerContentProps extends IsShowModalProps {
+  width: string;
+}
+
+export const CentralModal = ({
+  isShowModal,
+  setIsShowModal,
+  children,
+  width,
+}: ModalProps) => {
+  useEffect(() => {
+    if (isShowModal) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [isShowModal]);
+
+  return (
+    <Container>
+      <Background
+        isShowModal={isShowModal}
+        onClick={() => setIsShowModal(false)}
+      />
+      <ContainerContent isShowModal={isShowModal} width={width}>
+        {children}
+      </ContainerContent>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: relative;
+  z-index: 100;
+`;
+
+const Background = styled.div<IsShowModalProps>`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  visibility: ${({ isShowModal }) => (isShowModal ? 'visible' : 'hidden')};
+  background: rgba(46, 43, 41, 0.4);
+  transition: visibility 0.1s;
+`;
+
+const ContainerContent = styled.div<ContainerContentProps>`
+  position: fixed;
+  left: 50%;
+  bottom: 50%;
+  visibility: ${({ isShowModal }) => (isShowModal ? 'visible' : 'hidden')};
+  width: ${({ width }) => width};
+  padding: ${pxToRem(30)};
+  background-color: ${({ theme }) => theme.background.default};
+  transform: translate(-50%);
+`;
